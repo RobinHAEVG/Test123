fa2fd1e93fcefab8c50a7cd5bba9b3138a841627
Robin <robin.kaiser@haevg-rz.de>
2021-01-29 09:45:09 +0100

    nutzlosen kommentar entfernt

--- CollectionViewSource/MainWindow.xaml.cs
+++ CollectionViewSource/MainWindow.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace CollectionViewSource
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
